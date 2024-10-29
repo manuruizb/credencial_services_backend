af18db79db5f3e8eb6d204762f1a75fd00aaf87f
JuanDuque <jduqueg2@correo.tdea.edu.co>
2024-10-29 17:24:55 -0500

    Ajustando

--- Program.cs
+++ Program.cs
@@ -16,14 +16,36 @@ var app = builder.Build();
 
 app.UseCors(); // Habilita CORS
 
+var credenciales = new List<Credencial>();
+const string correctKey = "123456789";
+
 app.MapPost("/api/authenticate", ([FromBodyAttribute]string key) => {
     Console.WriteLine(key);
-    if (key == "123456789") {
-        var token = Convert.ToBase64String(Encoding.UTF32.GetBytes("123456789"));
-        return Results.Ok(token);
+    if (key == correctKey) {
+        return Results.Ok(true);
     }
 
-    return Results.Unauthorized();
+    return Results.Ok(false);
 });
 
+app.MapPost("/api/credenciales", (Credencial credencial) => {
+    credenciales.Add(credencial);
+
+    return Results.Ok();
+});
+
+app.MapGet("/api/credenciales", () => {
+
+    return Results.Ok(credenciales);
+
+});
+
+
 app.Run();
+
+public class Credencial
+{
+    public string servicio { get; set; }
+    public string usuario { get; set; }
+    public string password { get; set; }
+}
