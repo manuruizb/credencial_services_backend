186b1df9db77fdb99724a28be587d763440626e5
manuruizb <ruizbedoyamanuela@gmail.com>
2024-11-02 11:35:47 -0500

    Ultimo entregable

--- Program.cs
+++ Program.cs
@@ -19,25 +19,61 @@ app.UseCors(); // Habilita CORS
 var credenciales = new List<Credencial>();
 const string correctKey = "123456789";
 
-app.MapPost("/api/authenticate", ([FromBodyAttribute]string key) => {
+app.MapPost("/api/authenticate", ([FromBodyAttribute] string key) =>
+{
     Console.WriteLine(key);
-    if (key == correctKey) {
+    if (key == correctKey)
+    {
         return Results.Ok(true);
     }
 
     return Results.Ok(false);
 });
 
-app.MapPost("/api/credenciales", (Credencial credencial) => {
+app.MapPost("/api/credenciales", (Credencial credencial) =>
+{
+    foreach (var item in credenciales)
+    {
+        if(item.usuario.ToLower() == credencial.usuario.ToLower())
+        {
+            return Results.BadRequest("El nombre de usuario ya existe.");
+        }
+    }
     credenciales.Add(credencial);
 
     return Results.Ok();
 });
 
-app.MapGet("/api/credenciales", () => {
+app.MapGet("/api/credenciales", () =>
+{
 
     return Results.Ok(credenciales);
 
+});
+
+app.MapGet("/api/search", (string search) =>
+{
+    foreach (var item in credenciales)
+    {
+        if (item.servicio.ToLower() == search.ToLower() || item.usuario.ToLower() == search.ToLower())
+        {
+            return Results.Ok(item);
+        }
+    }
+    return Results.BadRequest("No se encontró.");
+});
+
+app.MapDelete("/api/delete", (string usuario) =>
+{
+    foreach (var item in credenciales)
+    {
+        if(item.usuario.ToLower() == usuario.ToLower())
+        {
+            credenciales.Remove(item);
+            return Results.Ok();
+        }
+    }
+    return Results.BadRequest("No se pudo eliminar. ");
 });
 
 
